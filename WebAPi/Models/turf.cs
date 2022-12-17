638f7060e6f0b86296396dface61ab5eff69fa33
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-12-17 16:11:15 +0100

    ASP + modal/context

---
+++ WebAPi/Models/turf.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPi.Models
+{
+    public class turf
+    {
+        public long id { get; set; }
+       public string? Drink { get; set; }
+        public int Price { get; set; }
+    }
+}
