88239e09238c9e85abbf209935f01ac5ff2e77e4
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-12-18 18:00:38 +0100

    Push before rshitty pc restart

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,11 +1,13 @@
-﻿using MauiApp1.ViewModel;
+﻿using CommunityToolkit.Mvvm.Input;
+using MauiApp1.Models;
+using MauiApp1.ViewModel;
 
 namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
-
+	// public TurfInfo TurfInfo { get; set; }
 	public MainPage(MainViewModel vm)
 	{
 		InitializeComponent();
--- MauiApp1/ViewModel/MainViewModel.cs
+++ MauiApp1/ViewModel/MainViewModel.cs
@@ -1,67 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MauiApp1.Models;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using MauiApp1.Services;
 
 namespace MauiApp1.ViewModel
 {
-    public partial class MainViewModel : ObservableObject
+    public partial class MainViewModel
     {
         public MainViewModel()
         {
-            Items = new ObservableCollection<string>();
-        }
-
-        [ObservableProperty]
-        ObservableCollection<string> items;
-        
-        [ObservableProperty]
-        string text;
-
-
-        // ICommand changed to RelayCommand
-        [RelayCommand]
-        void Add()
-        {
-            if (string.IsNullOrWhiteSpace(Text))
-                return;
-
-            Items.Add(text);
-            // Add new Turf
-            Text = string.Empty;
+          
         }
 
         [RelayCommand]
-        void Delete(String s)
+        public async void AddTurf()
         {
-            if(Items.Contains(s))
-            {
-                Items.Remove(s);
-            }
+           
+            
         }
-
-        [RelayCommand]
-        async Task Tap(String s)
-        {
-            await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
-
-            // For sending objects
-            /*
-            await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}",
-            new Dictionary<string, object>
-                {
-                    {nameof(DetailPage), new object()}
-                });
-            */
-
-        }
-
-        [RelayCommand]
-        async Task Edit(String s)
-        {
-            await Shell.Current.GoToAsync($"{nameof(EditPage)}?Text={s}");
-        }
-
 
     }
 }
