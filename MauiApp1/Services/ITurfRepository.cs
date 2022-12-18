5092fcaf6be8f090e058ac17486bc7f0c72b5a44
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-12-18 17:02:32 +0100

    pu,post,del

---
+++ MauiApp1/Services/ITurfRepository.cs
@@ -0,0 +1,17 @@
+﻿using MauiApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    internal interface ITurfRepository
+    {
+        Task<bool> AddUpdateTurfAsync(TurfInfo turf);
+        Task<bool> DeleteTurfAsync(int id);
+        Task<TurfInfo> GetTurfAsync(int id);
+        Task<IEnumerable<TurfInfo>> GetProductAsync();
+    }
+}
