16c3a145d44fbbb509fbe8d89c6f0e9154b55525
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-12-18 11:24:36 +0100

    api methods

--- WebAPi/Models/turf.cs
+++ WebAPi/Models/turf.cs
@@ -2,7 +2,7 @@
 {
     public class turf
     {
-        public long id { get; set; }
+        public long Id { get; set; }
        public string? Drink { get; set; }
         public int Price { get; set; }
     }
